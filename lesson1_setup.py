a12e7c4ddf2f7548d65a293c8667004343a1a5a2
Emanuele Disco <emawind84@gmail.com>
2017-07-29 22:57:48 +0900

    update

--- lesson1_setup.py
+++ lesson1_setup.py
@@ -2,7 +2,7 @@
 # You must change the IP address and your name in the create() call below before running!
 
 from mcpi import minecraft
-mc = minecraft.Minecraft.create(address="127.0.0.1", name="steve")
+mc = minecraft.Minecraft.create(address="127.0.0.1", name="Surfernight")
 
 y = mc.getHeight(0, 0)
 mc.setBlocks(-4, y-1, -4, 4, y+1, 4, 7)
--- script.py
+++ script.py
@@ -2,8 +2,25 @@ import mcpi.minecraft as minecraft
 import time
 
 # Connect to minecraft server 127.0.0.1 as player 'steve'. Replace with your own IP/name!
-mc = minecraft.Minecraft.create(address="127.0.0.1", name="steve")
+mc = minecraft.Minecraft.create(address="127.0.0.1", name="Surfernight")
 
 # PUT CODE HERE.
-# Checkout the /examples directory with ready-to-go scripts, all you need to do is update your IP and name in them!
+# Checkout the /examples directory with ready-to-go scripts, all you need to do is update your IP and name in them! 
 
+
+x = 110
+z = 14
+y = 64
+
+while True:
+    pos = mc.player.getTilePos()
+
+    print "x", pos.x
+    print "y", pos.y
+    print "z", pos.z
+
+    # If player position is same as teleportation pad...
+    if pos.x == x and pos.z == z and pos.y == y:
+        # We are on the teleportation pad!
+        mc.postToChat("Beam me up, Scotty!")
+    time.sleep(.1)
